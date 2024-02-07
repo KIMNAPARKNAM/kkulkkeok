480be3cc899a4085a2db9c0adbbc5fcd3c402a0c
dbfjru <dbfjru@gmail.com>
2024-02-07 20:40:23 +0900

    init commit

---
+++ src/main/java/io/kimnaparknam/kkulkkeok/KkulkkeokApplication.java
@@ -0,0 +1,13 @@
+package io.kimnaparknam.kkulkkeok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkulkkeokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkulkkeokApplication.class, args);
+    }
+
+}
