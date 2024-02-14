197fd80d0c439e2f187e3278a4e27490371510fe
kimpangya <nam74415133@gmail.com>
2024-02-14 19:16:46 +0900

    feat(회원가입 시 이메일, 닉네임 중복확인)

--- src/main/java/io/kimnaparknam/kkulkkeok/user/UserController.java
+++ src/main/java/io/kimnaparknam/kkulkkeok/user/UserController.java
@@ -1,6 +1,8 @@
 package io.kimnaparknam.kkulkkeok.user;
 
 import io.kimnaparknam.kkulkkeok.common.ResponseDto;
+import io.kimnaparknam.kkulkkeok.security.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,10 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,4 +37,27 @@ public class UserController {
 
         return ResponseEntity.status(HttpStatus.CREATED.value()).body(new ResponseDto<>("회원가입 성공", HttpStatus.CREATED.value(),null));
     }
+
+    //회원가입 -> 이메일 중복 확인
+    @GetMapping("/v/email/{email}")
+    public ResponseEntity<ResponseDto<Void>> checkEmail(@PathVariable(name = "email") @Valid String email) {
+        try {
+            userService.checkLoginEmail(email);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(new ResponseDto<>(e.getMessage(), 400,null));
+        }
+        return ResponseEntity.ok().body(new ResponseDto<>("사용 가능한 이메일 입니다.", 200,null));
+    }
+
+    //회원가입 -> 닉네임 중복확인
+    @GetMapping("/v/nickname/{nickname}")
+    public ResponseEntity<ResponseDto<Void>> checkNickname(@PathVariable(name = "nickname") String nickname) {
+        try {
+            userService.checkNickname(nickname);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(new ResponseDto<>(e.getMessage(), 400,null));
+        }
+        return ResponseEntity.ok().body(new ResponseDto<>("사용 가능한 닉네임 입니다.", 200,null));
+    }
+
 }
--- src/main/java/io/kimnaparknam/kkulkkeok/user/UserRequestDto.java
+++ src/main/java/io/kimnaparknam/kkulkkeok/user/UserRequestDto.java
@@ -4,8 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
 public class UserRequestDto {
-    String introduction;
-    String password;
+    private String introduction;
+    private String password;
 }
